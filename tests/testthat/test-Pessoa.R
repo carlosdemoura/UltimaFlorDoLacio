ac56ba0b749234c0a9daad245877205c1f4e8c2d
carlosdemoura <lcbarrosdemoura@gmail.com>
2025-03-08 18:19:19 -0300

    test Lupicinio: encoding options 2

--- tests/testthat/test-Pessoa.R
+++ tests/testthat/test-Pessoa.R
@@ -1,11 +1,11 @@
 test_that("Pessoa() works", {
-  old_encoding = getOption("encoding")
-  options(encoding = "UTF-8")
+  old_encoding = Sys.getlocale("LC_CTYPE")
+  Sys.setlocale("LC_CTYPE", "pt_BR.UTF-8")
   x = list(author = "Fernando Pessoa",
            title = "Argumento do jornalista",
            text = "Argumento do jornalista\n\nAs artes todas são uma futilidade perante a literatura. As artes que se ...",
            source = "http://arquivopessoa.net/textos/4075"
   )
   expect_equal(Pessoa(max.char = 100, seed = 0), x)
-  options(encoding = old_encoding)
+  Sys.setlocale("LC_CTYPE", old_encoding)
 })
