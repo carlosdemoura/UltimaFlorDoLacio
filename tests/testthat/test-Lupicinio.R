0fa33990a446d03d3144a43e1fed201e540b635a
carlosdemoura <lcbarrosdemoura@gmail.com>
2025-03-08 19:47:22 -0300

    test Lupicinio: encoding options 10

--- tests/testthat/test-Lupicinio.R
+++ tests/testthat/test-Lupicinio.R
@@ -1,7 +1,9 @@
 test_that("Lupicinio() works", {
   correct_character = function(x) {
     iconv(x, from = "latin1", to = "UTF-8") |>
-      {\(.) gsub('\\u0083Â', '', .)}()
+      {\(.) gsub('\u0083Â', '', .)}()
+      #{\(.) gsub('\\u0083Â', '', .)}()
+      #stringi::stri_replace_all_regex("\u0083Â", "")
   }
 
   expected = list(
@@ -10,17 +12,25 @@ test_that("Lupicinio() works", {
     text   = "Quem vê aquele carteiro\nO dia inteiro feliz a cantar\nDeixando missivas diversas\nEm cada lugar\n\nNão pode julgar se ele sabe\nQue a pasta onde cabem\nAs cartas de amor\nTrazem junto as notícias de luto\nInfelicidade, infortunas e dor\n\nAquele carteiro que sempre me trouxe\nNotícias do meu grande bem\nO mesmo carteiro, hoje vem me avisar\nQue quem amo tem um outro alguém\nE a cantar bem contente\nSai indiferente\nSem ver que eu fico a chorar\nO carteiro devia saber\nO que vai entregar",
     source = "https://www.letras.mus.br/lupcinio-rodrigues/1410841/"
   ) |>
-    sapply(correct_character)
-    #sapply(function(x) iconv(x, from = "latin1", to = "UTF-8"))
+    sapply(function(x) {
+      x |>
+        iconv(from = "latin1", to = "UTF-8") |>
+        iconv(from = "UTF-8", "ASCII", sub = "")})
+    #sapply(correct_character)
     #sapply(function(x) stringi::stri_encode(x, from = "", to = "UTF-8") )
     #sapply(function(x) enc2native(x))
 
 
-  real = Lupicinio(seed = 0) |>
-    sapply(correct_character)
+  actual = Lupicinio(seed = 0) |>
+    sapply(function(x) {
+      x |>
+        iconv(from = "latin1", to = "UTF-8") |>
+        iconv(from = "UTF-8", "ASCII", sub = "")})
+    #sapply(function(x) iconv(x, from = "latin1", to = "UTF-8"))
+    #sapply(function(x) x)
     #sapply(function(x) iconv(x, from = "latin1", to = "UTF-8"))
     #sapply(function(x) stringi::stri_encode(x, from = "", to = "UTF-8") )
     #sapply(function(x) enc2native(x))
 
-  expect_equal(real, expected)
+  expect_equal(actual, expected)
 })
