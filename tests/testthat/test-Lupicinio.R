05758bffe5156f93cf5343dc0e9d83209a3b97f2
carlosdemoura <lcbarrosdemoura@gmail.com>
2025-03-08 18:29:09 -0300

    test Lupicinio: encoding options 3

--- tests/testthat/test-Lupicinio.R
+++ tests/testthat/test-Lupicinio.R
@@ -1,8 +1,11 @@
 test_that("Lupicinio() works", {
+  old_encoding = getOption("encoding")
+  options(encoding = "UTF-8")
   x = list(author = "Lupicínio Rodrigues",
            title = "Carteiro",
            text = "Quem vê aquele carteiro\nO dia inteiro feliz a cantar\nDeixando missivas diversas\nEm cada lugar\n\nNão ...",
            source = "https://www.letras.mus.br/lupcinio-rodrigues/1410841/"
   )
   expect_equal(Lupicinio(max.char = 100, seed = 0), x)
+  options(encoding = old_encoding)
 })
